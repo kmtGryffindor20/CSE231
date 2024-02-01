4d8a7b2107dd718884875f326a843ca571b2101e
Knox <tripathikmt20@gmail.com>
2024-02-01 15:23:52 +0530

    [UPDATED] Made the threads mutually exclusive and separate threads for each number

--- fibonacci_threads.c
+++ fibonacci_threads.c
@@ -4,12 +4,15 @@
 
 int a = 0;
 int b = 1;
+pthread_mutex_t lock;
 
 void *runner() {
+    pthread_mutex_lock(&lock);
     int c = a + b;
-    printf("%d\n", a);
+    printf("Thread ID: %ld, Fibonacci Number: %d\n", pthread_self(), a);
     a = b;
     b = c;
+    pthread_mutex_unlock(&lock);
     pthread_exit(0);
 }
 
@@ -24,12 +27,16 @@ int main(int argc, char *argv[])
         return -1;
     }
     int i = atoi(argv[1]);
-    while (i--)
-    {
-        pthread_t tid;
-        pthread_attr_t attr;
-        pthread_attr_init(&attr);
-        pthread_create(&tid,&attr,runner,NULL);
-        pthread_join(tid,NULL);
+    pthread_t tid[i];
+    pthread_attr_t attr;
+    pthread_attr_init(&attr);
+    pthread_mutex_init(&lock, NULL);
+    for (int j = 0; j < i; j++) {
+        pthread_create(&tid[j], &attr, runner, NULL);
     }
+    for (int j = 0; j < i; j++) {
+        pthread_join(tid[j], NULL);
+    }
+    pthread_mutex_destroy(&lock);
+    return 0;
 }
