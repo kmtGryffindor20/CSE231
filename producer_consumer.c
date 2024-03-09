6ee4c01352c526411ab663fb1d9cf31ad7088948
Knox <tripathikmt20@gmail.com>
2024-03-09 15:51:57 +0530

    [UPDATED] Multiple producers and consumers

--- producer_consumer.c
+++ producer_consumer.c
@@ -47,17 +47,34 @@ void *consumer(void *arg) {
     }
 }
 
-int main() {
+int main(int argc, char* argv[]) {
     pthread_t producer_thread, consumer_thread;
     sem_init(&empty, 0, BUFFER_SIZE);
     sem_init(&full, 0, 0);
     sem_init(&mutex, 0, 1);
 
-    pthread_create(&producer_thread, NULL, producer, NULL);
-    pthread_create(&consumer_thread, NULL, consumer, NULL);
+    if(argc != 3)
+    {
+        printf("Usage: %s <num_producers> <num_consumers>\n", argv[0]);
+        return 1;
+    }
 
-    pthread_join(producer_thread, NULL);
-    pthread_join(consumer_thread, NULL);
+    int num_producers = atoi(argv[1]);
+    int num_consumers = atoi(argv[2]);
+
+    for (int i = 0; i < num_producers; i++) {
+        pthread_create(&producer_thread, NULL, producer, NULL);
+    }
+    for (int i = 0; i < num_consumers; i++) {
+        pthread_create(&consumer_thread, NULL, consumer, NULL);
+    }
+
+    for(int i = 0; i < num_producers; i++) {
+        pthread_join(producer_thread, NULL);
+    }
+    for(int i = 0; i < num_consumers; i++) {
+        pthread_join(consumer_thread, NULL);
+    }
 
     sem_destroy(&empty);
     sem_destroy(&full);
